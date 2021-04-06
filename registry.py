922f18fa6bc55249bcc57d7403c50de0263f8266
Puillet <pierreguillet99@gmail.com>
2021-04-06 09:17:59 +0200

    little rewrite from Sprint 1

--- registry.py
+++ registry.py
@@ -2,11 +2,9 @@ print("Hello World")
 
 codeStateTab = ["FR", "UK", "DE", "IT", "ES"]
 tvaValueTab = [20, 15, 19, 7, 10]
-i = 0
+
 for elements in codeStateTab:
     print(elements)
-
-print(codeStateTab[0])
 
 print ("--------------")
 montantHT = input("Saississez un montant H.T. \n")
