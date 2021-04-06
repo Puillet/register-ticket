6c4951b6001908ef61b63b135937b68e2fcf8674
Puillet <pierreguillet99@gmail.com>
2021-04-06 09:17:21 +0200

    Sprint 1

--- registry.py
+++ registry.py
@@ -1,5 +1,18 @@
 print("Hello World")
 
-codeStateTab = [["FR", "UK", "DE", "IT", "ES"], [20, 15, 19, 7, 10]]
+codeStateTab = ["FR", "UK", "DE", "IT", "ES"]
+tvaValueTab = [20, 15, 19, 7, 10]
+i = 0
+for elements in codeStateTab:
+    print(elements)
 
-print(str(codeStateTab))
+print(codeStateTab[0])
+
+print ("--------------")
+montantHT = input("Saississez un montant H.T. \n")
+print ("--------------")
+montantTVA = input("Saississez un montant de TVA \n")
+
+montantTTC = int(montantHT) + int(montantTVA)
+print ("--------------")
+print("Le montant TTC est égale à", montantTTC)
